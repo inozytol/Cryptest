b530e4c9e5b4dc2873a3ce3176f15b6e83eb1261
gN <inozytol@gmail.com>
2018-12-02 00:36:46 +0100

    Added empty test for Junit 5

---
+++ TestClass.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClass {
+
+    @Test
+    void testPass() {
+        assertEquals(14,14);
+    }
+}
+
--- run.sh
+++ run.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
-rm foo*
+# 20181201, based on: https://stackoverflow.com/questions/52373469/how-to-launch-junit-5-platform-from-the-command-line-without-maven-gradle
 
+rm foo*
+rm -fr target # remove directory, forcefully (with contents, and without asking. DO NOT TRY THIS AT HOME (or more importantly at /)
+mkdir target
+
+
+# This is some text file for test purposes
 echo "Hello and goodbye" > foo
 
-javac Cryptest.java
+result=0 # contains sum of exit codes from various commands. Zero if everything successful
 
-if [ "$?" -eq "0" ]
+javac -d target Cryptest.java
+let "result=$result+$?"
+
+# Test compilation requires this classpath as it contains imports from junit jupiter
+javac -d target -cp ~/APPS/java/junit/junit-platform-console-standalone-1.3.2.jar TestClass.java
+let "result=$result+$?"
+
+if [ "$result" -eq "0" ]
 then
     echo "Compilation completed"
     echo "Running tests"
-    java Cryptest
+    java -jar ~/APPS/java/junit/junit-platform-console-standalone-1.3.2.jar --class-path target --scan-class-path # it searches for tests in compiled classes from given directory
 else
-    echo "Compilation failed \n"
+    echo "Compilation failed"
 fi
